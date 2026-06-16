18ede6c3ec385549fbdfaaeacc7915c6fb23aa03
Winxx0102 <olowixtovar@gmail.com>
2026-06-16 18:21:06 -0400

    supaass

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,8 @@ const nextConfig: NextConfig = {
           {
             key: 'Content-Security-Policy',
 // En tu next.config.ts
-value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self' https://revista-cientifica-by-winxx0102.onrender.com",
+// En tu next.config.ts
+value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data:; connect-src 'self' https://revista-cientifica-by-winxx0102.onrender.com https://citlayiapryuepjhdofv.supabase.co",
           },
         ],
       },
