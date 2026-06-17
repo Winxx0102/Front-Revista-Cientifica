7ffb41c675fc10c2ea63e027cfa7329615c2d1a9
Winxx0102 <olowixtovar@gmail.com>
2026-06-16 20:17:31 -0400

    archisss

--- next.config.ts
+++ next.config.ts
@@ -8,9 +8,8 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-// En tu next.config.ts
-// En tu next.config.ts
-value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data:; connect-src 'self' https://revista-cientifica-by-winxx0102.onrender.com https://citlayiapryuepjhdofv.supabase.co",
+
+value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data:; connect-src 'self' https://revista-cientifica-by-winxx0102.onrender.com https://citlayiapryuepjhdofv.supabase.co;frame-src 'self' https://citlayiapryuepjhdofv.supabase.co;",
           },
         ],
       },
