55280fe0cbdd6294254e7283763bbba5b5a72af9
Winxx0102 <olowixtovar@gmail.com>
2026-06-26 17:56:22 -0400

    navbarcleannsss

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,10 +2,10 @@
 
 import ProtectedRoute from "@/components/ProtectedRoute";
 
-export default function NormasPage() {
+export default function AboutPage() {
   return (
      <ProtectedRoute>   <div className="max-w-6xl mx-auto px-6 py-8">
-      <h1 className="text-3xl font-bold text-white mb-6">Normas</h1>
+      <h1 className="text-3xl font-bold text-white mb-6">Acerca de</h1>
       <p className="text-slate-300">
         Aquí va la info... mision, visión, objetivos, etc. de la revista científica.
       </p>
